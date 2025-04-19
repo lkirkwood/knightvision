c303a2a1511af41876b34e43fd6ec94a42a0c6c0
Linus Kirkwood <linuskirkwood@gmail.com>
2025-04-19 20:35:00 +1000

    Finish main activity

--- app/src/main/java/com/knightvision/MainActivity.kt
+++ app/src/main/java/com/knightvision/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.knightvision
 
 import android.os.Bundle
+import android.os.Build
 import android.view.ViewGroup
 import android.content.Context
 import android.content.pm.PackageManager
 import android.Manifest
 import android.widget.Toast
+import android.graphics.Color
+import android.graphics.Bitmap
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.OutlinedButton
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalContext
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.core.Preview
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.core.app.ActivityCompat
 import androidx.activity.compose.setContent
 import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.newSingleThreadContext
+import java.util.concurrent.Executors
+import java.io.ByteArrayOutputStream
+import com.knightvision.AnalysisActivity
+import android.content.Intent
 
 @Composable
-fun CameraPreview(cameraProvider : ProcessCameraProvider) {
+fun CameraPreview(cameraProvider : ProcessCameraProvider, onPictureTaken : (ImageProxy) -> Unit) {
+    val capture = ImageCapture.Builder().build()
+
     AndroidView(
         factory = { context ->
             val previewView = PreviewView(context).apply {
@@ -43,23 +65,30 @@ fun CameraPreview(cameraProvider : ProcessCameraProvider) {
             cameraProvider.bindToLifecycle(
                 context as LifecycleOwner,
                 cameraSelector,
-                preview
+                preview,
+                capture
             )
 
             previewView
         }
     )
+
+    val cameraExecutor = Executors.newSingleThreadExecutor()
+    val context = LocalContext.current
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedButton(shape = CircleShape, onClick = {
+            capture.takePicture(cameraExecutor, object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image : ImageProxy) {
+                    onPictureTaken(image)
+                }
+
+                override fun onError(exception : ImageCaptureException) {
+                    Toast.makeText(context, exception.toString(), Toast.LENGTH_LONG).show()
+                }
+            })
+        }) {}
+    }
 }
-
-// @Composable
-// fun MainMenu(cameraProvider: ProcessCameraProvider) {
-//     Column {
-//         Button(onClick = {CameraPreview(cameraProvider)}) {
-//             Text("Take a picture")
-//         }
-//     }
-// }
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
@@ -74,7 +103,16 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture.addListener(Runnable {
             cameraProvider = cameraProviderFuture.get()
             setContent {
-                CameraPreview(cameraProvider)
+                CameraPreview(cameraProvider, {image : ImageProxy -> Unit
+
+                    val stream = ByteArrayOutputStream()
+                    image.toBitmap().compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    val byteArray = stream.toByteArray()
+
+                    val intent = Intent(this, AnalysisActivity::class.java)
+                    intent.putExtra("image_bytes", byteArray)
+                    this.startActivity(intent)
+                })
             }
         }, ContextCompat.getMainExecutor(this))
 
