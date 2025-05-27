91ff3c3313d365ba304cde42cf10b55b2f91e732
Linus Kirkwood <linuskirkwood@gmail.com>
2025-05-27 21:20:20 +1000

    Remove bestmove + add validFen to stockfishbridge

--- app/src/main/java/com/knightvision/StockfishBridge.kt
+++ app/src/main/java/com/knightvision/StockfishBridge.kt
@@ -6,17 +6,14 @@ object StockfishBridge {
     }
 
     var enginePtr: Long? = null
-    var bestmoveOutputPtr: Long? = null
 
-    private external fun _initEngine(): LongArray
+    private external fun _initEngine(): Long
     private external fun runCmd(enginePtr: Long, cmd: String): String
-    private external fun bestmove(enginePtr: Long, outputPtr: Long): String
-    private external fun goBlocking(enginePtr: Long, outputPtr: Long, depth: Int): String
+    private external fun goBlocking(enginePtr: Long, depth: Int): String
+    external fun validFen(fen: String): Boolean
 
     fun initEngine() {
-        val pointers = _initEngine()
-        enginePtr = pointers[0]
-        bestmoveOutputPtr = pointers[1]
+        enginePtr = _initEngine()
 
         // runCmd("setoption name Threads value 1")
     }
@@ -30,17 +27,10 @@ object StockfishBridge {
         return runCmd(this.enginePtr!!, cmd)
     }
 
-    fun bestmove(): String {
-        if (this.enginePtr == null || this.bestmoveOutputPtr == null) {
-            throw RuntimeException("Trying to get best move from stockfish before initialising engine.")
-        }
-        return bestmove(this.enginePtr!!, this.bestmoveOutputPtr!!)
-    }
-
     fun goBlocking(depth: Int): String {
-        if (this.enginePtr == null || this.bestmoveOutputPtr == null) {
+        if (this.enginePtr == null) {
             throw RuntimeException("Trying to search game tree before initialising engine.")
         }
-        return goBlocking(this.enginePtr!!, this.bestmoveOutputPtr!!, depth)
+        return goBlocking(this.enginePtr!!, depth)
     }
 }
