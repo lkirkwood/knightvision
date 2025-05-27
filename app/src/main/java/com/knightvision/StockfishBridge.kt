029d5da3804b6e755b0ca3a490573b8b39f3d68e
Linus Kirkwood <linuskirkwood@gmail.com>
2025-05-27 15:39:09 +1000

    Add blocking stockfish bridge method

--- app/src/main/java/com/knightvision/StockfishBridge.kt
+++ app/src/main/java/com/knightvision/StockfishBridge.kt
@@ -8,19 +8,17 @@ object StockfishBridge {
     var enginePtr: Long? = null
     var bestmoveOutputPtr: Long? = null
 
-    // Issue with code is that on_bestmove is called on static instance of UCIEngine with no bestmove_output value
-    // just pass around pointer to ostream instead
-
     private external fun _initEngine(): LongArray
     private external fun runCmd(enginePtr: Long, cmd: String): String
     private external fun bestmove(enginePtr: Long, outputPtr: Long): String
+    private external fun goBlocking(enginePtr: Long, outputPtr: Long, depth: Int): String
 
     fun initEngine() {
         val pointers = _initEngine()
         enginePtr = pointers[0]
         bestmoveOutputPtr = pointers[1]
 
-        runCmd("setoption name Threads value 1")
+        // runCmd("setoption name Threads value 1")
     }
 
     // TODO add thread safety to this object
@@ -37,5 +35,12 @@ object StockfishBridge {
             throw RuntimeException("Trying to get best move from stockfish before initialising engine.")
         }
         return bestmove(this.enginePtr!!, this.bestmoveOutputPtr!!)
+    }
+
+    fun goBlocking(depth: Int): String {
+        if (this.enginePtr == null || this.bestmoveOutputPtr == null) {
+            throw RuntimeException("Trying to search game tree before initialising engine.")
+        }
+        return goBlocking(this.enginePtr!!, this.bestmoveOutputPtr!!, depth)
     }
 }
